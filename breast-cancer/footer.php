6c1b5701d44c53d8d263f1ae4f9915a494abf287
vishal02017bhu-hash <vishal02017bhu@gmail.com>
2026-05-06 23:11:11 +0530

    Delete breast-cancer directory

--- breast-cancer/footer.php
+++
@@ -1,6 +0,0 @@
-<footer>
-    <p>© 2026 Breast Cancer Awareness System</p>
-</footer>
-<script src="/BREAST-CANCER/assets/script.js"></script>
-</body>
-</html>
--- breast-cancer/includes/db.php
+++
@@ -1,7 +0,0 @@
-<?php
-$conn = mysqli_connect("localhost", "root", "", "breast_cancer_system");
-
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
-}
-?>
